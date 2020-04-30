3bf4d5285cc30558a0d1ab6ce9b3f896ce6e0845
Lena Mangold <lena@mangold.org>
2020-04-29 20:35:52 -0500

    modify test app

--- sankeyplot_app_test.R
+++ sankeyplot_app_test.R
@@ -8,46 +8,47 @@ library("ggalluvial")
 library("ggparallel")
 library("googleVis")
 
+###=================================================================================
+### define factor levels to specify order of strata
+fct_levels <- c("Labour", "Conservative", "Liberal Democrat", "UKIP", 
+                "Brexit Party", "Green Party", "SNP", "Plaid Cymru")
+
+###=================================================================================
 ### define plot colours
-colours = c("#DC241f", "#0087DC", "#FAA61A", "#70147A", "#12B6CF", 
-            "#528D6B", "#FDF38E", "#008142")
-names(colours) = c("Labour", "Conservative", "Liberal Democrat", "UKIP", "Brexit Party", 
-                   "Green Party", "SNP", "Plaid Cymru")
-alpha <- 0.7 # transparency value
+colours <- c("#DC241f", "#0087DC", "#FAA61A", "#70147A", 
+             "#12B6CF", "#528D6B", "#FDF38E", "#008142")
+names(colours) <- fct_levels
 
-# define factor levels
-fct_levels <- c("Labour", "Conservative", "Liberal Democrat", "UKIP", "Brexit Party", "Green Party", "SNP", "Plaid Cymru")
-fct_levels_17 <- paste0(fct_levels, " 17")
-fct_levels_19 <- paste0(fct_levels, " 19")
+###=================================================================================
+### format data
+dat_17_19 <- read.csv("sankey_data.csv")
 
-### create data for sankey plots
-dat_17_19 = read.csv("sankey_data.csv")
-
-dat_17_19_ggalluvial = dat_17_19 %>% 
+dat_17_19_ggalluvial <- dat_17_19 %>% 
   mutate_at(vars(vote17, vote19),
             funs(factor(., levels = fct_levels)))
 
 dat_17_19_ggforce <- dat_17_19  %>%
   gather_set_data(1:2) %>%
   arrange(x,vote19,desc(vote17)) %>% 
-  mutate_at(vars(vote17, vote19),
+  mutate_at(vars(vote17, vote19, y),
             funs(factor(., levels = fct_levels)))
 
-dat_17_19_googleVis = dat_17_19 %>% 
-  mutate_at(vars(vote17, vote19),
-            funs(factor(., levels = fct_levels))) %>%
-  mutate(vote17 = paste0(vote17, " 17"), 
-         vote19 = paste0(vote19, " 19")) %>% 
-  arrange(factor(vote17, levels = fct_levels_17), 
-          factor(vote19, levels = fct_levels_19)) %>% 
-  as.data.frame()
+###=================================================================================
+### define plot settings
+stratum_width <- 0.3
+stratum_font_colour <- "black"
+stratum_fill_colour <- "white"
+stratum_line_colour <- "black"
+stratum_angle <- 0
+legend_position <- ""
+alpha <- 0.7
 
 
 # Define UI for data download app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Downloading Data"),
+  titlePanel("Choose package for Sankey plot"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -91,40 +92,58 @@ server <- function(input, output) {
   
   plot <- reactive({
     if(input$package == "ggalluvial"){
-      gg <- ggplot(plot_data(),
+      gg <- ggplot(dat_17_19_ggalluvial,
                    aes(y = freq, axis1 = vote17, axis2 = vote19)) +
         geom_alluvium(aes(fill = vote17, color = vote17),
-                      width = 1/12, alpha = alpha, knot.pos = 0.4) +
-        geom_stratum(width = 1/6) +
-        ggfittext::geom_fit_text(infer.label = TRUE, stat = "stratum", width = 1/6, min.size = 3) +
-        scale_fill_manual(values  = colours) +
+                      width = stratum_width, 
+                      alpha = alpha, 
+                      knot.pos = 0.4) +
+        geom_stratum(width = stratum_width,
+                     fill = stratum_fill_colour, 
+                     colour = stratum_line_colour) +
+        ggfittext::geom_fit_text(infer.label = TRUE, 
+                                 stat = "stratum", 
+                                 width = stratum_width,
+                                 colour = stratum_font_colour,
+                                 fontface = "bold",
+                                 min.size = 3) +
+        scale_fill_manual(values = colours) +
         scale_color_manual(values = colours) +
         scale_x_continuous(breaks = 1:2, labels = c("GE vote 2017", "GE vote 2019")) +
         theme_minimal() +
         theme(
-          legend.position = "none",
-          panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank(),
-          axis.text.y = element_blank(),
-          axis.text.x = element_text(size = 14, face = "bold")
-        )
-    } else if(input$package == "ggforce") {
-      gg <- ggplot(plot_data(), aes(x = x, id = id, split = y, value = freq)) +
-        geom_parallel_sets(aes(fill = vote19), alpha = alpha, axis.width = 0.2,
-                           n=100, strength = 0.5) +
-        geom_parallel_sets_axes(axis.width = 0.25, fill = "gray95",
-                                color = "gray80", size = 0.15) +
-        geom_parallel_sets_labels(colour = 'gray35', size = 4.5, angle = 0, fontface="bold") +
-        scale_fill_manual(values  = colours) +
-        scale_color_manual(values = colours) +
-        theme_minimal() +
-        theme(
-          legend.position = "none",
+          legend.position = legend_position,
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           axis.text.y = element_blank(),
-          axis.text.x = element_text(size = 20, face = "bold"),
-          axis.title.x  = element_blank()
+          axis.text.x = element_text(size = 14, face = "bold"),
+          axis.title  = element_blank()
+        )
+    } else if(input$package == "ggforce") {
+      gg <- ggplot(dat_17_19_ggforce, aes(x = x, id = id, split = y, value = freq)) +
+        geom_parallel_sets(aes(fill = vote17), 
+                           alpha = alpha, 
+                           axis.width = stratum_width,
+                           n=1000) +
+        geom_parallel_sets_axes(axis.width = stratum_width, 
+                                fill = stratum_fill_colour,
+                                color = stratum_line_colour, 
+                                size = 0.3) +
+        geom_parallel_sets_labels(colour = stratum_font_colour, 
+                                  size = 3, 
+                                  angle = stratum_angle, 
+                                  fontface="bold") +
+        scale_fill_manual(values  = colours) +
+        scale_color_manual(values = colours) +
+        scale_x_discrete(labels = c("GE vote 2017", "GE vote 2019")) +
+        theme_minimal() +
+        theme(
+          legend.position = legend_position,
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.text.y = element_blank(),
+          axis.text.x = element_text(size = 14, face = "bold"),
+          axis.title  = element_blank()
         )
     }
     gg
@@ -136,13 +155,14 @@ server <- function(input, output) {
     plot()
   })
   
-  # Downloadable csv of selected dataset ----
+  # Downloadable png of selected plot type ----
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste(input$dataset, ".csv", sep = "")
+      paste0(input$package, ".png")
     },
     content = function(file) {
-      write.csv(plot_data(), file, row.names = FALSE)
+      plot()
+      ggsave(file, device = "png")
     }
   )
   
